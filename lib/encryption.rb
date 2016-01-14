3026ebafa7992dc6fed0c7ce1b498d8479d33563
Jessica Sumner <jess.r.sumner@gmail.com>
2016-01-14 14:55:20 -0500

    Encrypt method working
    First test is passing with a basic encryption with a set shift.
    Encrypt method uses the index value of a letter in an alphabet array and adds to it. It then calls the object in the alphabet array based on the new index.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,2 +1,14 @@
 class Encryption
+
+  def alphabet
+    alphabet = ("a".."z").to_a
+  end
+
+  def encrypt(string)
+    cypher = string.split("").map do |letter|
+      index = alphabet.find_index(letter) + 5
+      alphabet[index]
+    end
+    cypher.join
+  end
 end
